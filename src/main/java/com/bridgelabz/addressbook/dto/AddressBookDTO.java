26040b9d152364475bf0fa0e43f180fc4f2d5850
manjugoutham <gouthamy219@gmail.com>
2022-02-15 07:37:20 +0530

    Introducing DTO and Model to AddressBook App

--- src/main/java/com/bridgelabz/addressbook/dto/AddressBookDTO.java
+++ src/main/java/com/bridgelabz/addressbook/dto/AddressBookDTO.java
@@ -3,6 +3,7 @@ package com.bridgelabz.addressbook.dto;
 import com.sun.istack.NotNull;
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 @Data
@@ -16,11 +17,19 @@ public class AddressBookDTO {
     @NotNull
     @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$",message = "Person name is Invalid")
     private String name;
+
+    @Pattern(regexp = "[a-zA-Z0-9]+\\.[a-zA-Z0-9]+@gmail\\.com", message = "Person gmail is Invalid")
     private String emailId;
+    @NotBlank(message = "PhoneNumber cannot be Empty")
     private String phoneNumber;
+
+    @NotBlank(message = "address cannot be Empty")
     private String address;
+    @NotBlank(message = "city cannot be Empty")
     private String city;
+    @NotBlank(message = "state cannot be Empty")
     private String state;
+    @NotBlank(message = "zipCode cannot be Empty")
     private String zipCode;
 
 }
--- src/main/java/com/bridgelabz/addressbook/exceptions/AddressBookExceptionalHandler.java
+++ src/main/java/com/bridgelabz/addressbook/exceptions/AddressBookExceptionalHandler.java
@@ -25,4 +25,11 @@ public class AddressBookExceptionalHandler {
         return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
 
     }
+    @ExceptionHandler(AddressBookException.class)
+    public ResponseEntity<ResponseDTO> handleEmployeePayrollException(
+            AddressBookException exception){
+        ResponseDTO responseDTO = new ResponseDTO("Exception while processing Bad REST Request",
+                exception.getMessage());
+        return new ResponseEntity<ResponseDTO>(responseDTO,HttpStatus.BAD_REQUEST);
+    }
 }
